91a1e4a356f82b525c7350884e46e753635e1f39
NiklasEckert <niklas.eckert.1807@gmail.com>
2017-05-02 06:32:05 +0200

    An Netzwerkfähigkeit weiter gearbeitet

--- app/src/main/java/fes/profession/partygamecompanion/BottleCreateGroupActivity.java
+++ app/src/main/java/fes/profession/partygamecompanion/BottleCreateGroupActivity.java
@@ -1,13 +1,67 @@
 package fes.profession.partygamecompanion;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BottleCreateGroupActivity extends AppCompatActivity {
 
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+    BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
+
+    private boolean isWifiP2pEnabeld = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottle_create_group);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WifiDirectBroadcastReceiver(mManager, mChannel, this);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                
+            }
+
+            @Override
+            public void onFailure(int reason) {
+
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+    public void setIsWifiP2pEnabeld(boolean isWifiP2pEnabeld) {
+        this.isWifiP2pEnabeld = isWifiP2pEnabeld;
+    }
+
+    public boolean getIsWifiP2pEnabeld() {
+        return this.isWifiP2pEnabeld;
     }
 }
--- app/src/main/java/fes/profession/partygamecompanion/MainActivity.java
+++ app/src/main/java/fes/profession/partygamecompanion/MainActivity.java
@@ -14,20 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
     private final IntentFilter intentFilter = new IntentFilter();
 
-    WifiP2pManager.Channel mChannel;
-    WifiP2pManager mManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        mChannel = mManager.initialize(this, getMainLooper(), null);
     }
 
     public void startBottle(View view) {
